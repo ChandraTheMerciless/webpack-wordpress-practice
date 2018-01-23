f2942545ca5cc0fe7255fad4d332548d699c6b4c
Chandra <chandra.asar@gmail.com>
2018-01-23 09:55:28 -0500

    cleaning up master branch

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import CoolCat from './coolCat.png';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
-  // Lodash, imported by this script
   element.innerHTML = _.join(['Hello', 'webpack', '!!!'], ' ');
-  element.classList.add('hello');
-
-  // Add image to existing div
-  var cat = new Image();
-  cat.src = CoolCat;
-
-  element.appendChild(cat);
-
-  console.log(Data);
 
   return element;
 }
